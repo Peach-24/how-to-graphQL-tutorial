a6bdcb963e619afede4fc1ec06f12091364c68da
Josh Peachey <joshpeachey24@gmail.com>
2021-02-11 15:07:32 +0000

    Required in resolvers to index.js pre-AUTH testing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 const { apolloServer, ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // feed: async (parent, args, context, info) => {
-    //   return context.prisma.link.findMany();
-    // },
-  },
-  Mutation: {
-    // post: (parent, args, context, info) => {
-    //   const newLink = context.prisma.link.create({
-    //     data: {
-    //       url: args.url,
-    //       description: args.description,
-    //     },
-    //   });
-    //   return newLink;
-    // },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 const fs = require("fs");
