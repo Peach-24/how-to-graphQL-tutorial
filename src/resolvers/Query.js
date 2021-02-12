404d53bc7ddda7c6ea5f858816984a216b69552a
Josh Peachey <joshpeachey24@gmail.com>
2021-02-12 11:03:59 +0000

    Made feed func (filter) async, corrected type. Filtering now working

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,20 @@
-const feed = (parent, args, context, info) => {
-  const where = args.filter ? {
-    OR: [{ description: { contains: args.filter } }, { url: { contains: args.filter } }],
-  } : {}
-  
+const feed = async (parent, args, context, info) => {
+  // If no filter string is provided, then the where object will be just an empty object and no filtering conditions will be applied by Prisma Client when it returns the response for the links query.
+
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
   const links = await context.prisma.link.findMany({
     where,
-  })
+  });
 
-  return links
+  return links;
 };
 
 module.exports = {
