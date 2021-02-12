16799b9afca7221e2ae51285d3975c29035a7276
Josh Peachey <joshpeachey24@gmail.com>
2021-02-12 10:59:38 +0000

    Added filter to graphQL schema and updated Query.js with filter logic

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,13 @@
 const feed = (parent, args, context, info) => {
-  return context.prisma.link.findMany();
+  const where = args.filter ? {
+    OR: [{ description: { contains: args.filter } }, { url: { contains: args.filter } }],
+  } : {}
+  
+  const links = await context.prisma.link.findMany({
+    where,
+  })
+
+  return links
 };
 
 module.exports = {
